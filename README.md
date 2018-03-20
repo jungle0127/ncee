51667ba1db13276162a06861a3b965091a2b5804
jungle0127 <arbor@foxmail.com>
2018-03-20 22:23:41 +0800

    added new branch for this project based on springboot

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # ncee
 
-Spring + SpringMVC + Mybatis Project
+Springboot + MyBatis + Angular4
+
+## maven surefire plugin 使用
+1. mvn clean compile findbugs:findbugs    清理原有的字节码文件，并生成findbugs XML报告
+   mvn findbugs:gui  打开findbugs GUI工具查看扫描结果
+2. mvn clean compile surefire-report:report 生成findbugs HTML报告
+
+## maven Jacoco
+mvn clean package
+
+## Java代码质量检测
+
+http://blog.csdn.net/zzq900503/article/details/49616959
+
+findbugs, PMD, CheckStyle
--- src/main/java/com/ncee/controller/UsersController.java
+++ src/main/java/com/ncee/controller/UsersController.java
@@ -1,41 +1,36 @@
 package com.ncee.controller;
 
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ncee.dao.model.Users;
 import com.ncee.service.IUserService;
 
-@Controller
+@RestController
+@RequestMapping("/users")
 public class UsersController {
 	private final static Logger logger = LogManager.getLogger(UsersController.class);
 	@Autowired
 	@Qualifier("userService")
 	private IUserService usersService;
 	
-	@RequestMapping(value="/login",method={RequestMethod.POST})
-	public ModelAndView login(@RequestParam("loginName") String loginName, @RequestParam("password") String password,ModelAndView mv,HttpSession session){
-		System.out.println("UserController login method.");
+	@GetMapping("/user/{loginName}/{password}}")
+	public boolean login(@PathVariable String loginName,@PathVariable String password){
+		logger.info("login method");
 		Users user = this.usersService.login(loginName, password);
 		if(null != user){
 			logger.info("Login success with user:{}",user.getUsername());
-			mv.setViewName("welcome");
-			return mv;
+			return true;
 		}
 		else{
-			mv.addObject("message","User name or password invalid.");
-			mv.setViewName("index");
-			return mv;
+			return false;
 		}
 	}
 }
