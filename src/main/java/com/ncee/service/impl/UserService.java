ba9d2a288ccad30d7ee6eff7817e719442a680a3
jungle0127 <arbor@foxmail.com>
2018-03-24 12:54:01 +0800

    added angular web

--- src/main/java/com/ncee/service/impl/UserService.java
+++ src/main/java/com/ncee/service/impl/UserService.java
@@ -2,6 +2,8 @@ package com.ncee.service.impl;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import com.ncee.service.IUserService;
 
 @Service("userService")
 public class UserService implements IUserService {
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 	@Autowired
 	private UsersMapper userMapper;
 	@Override
@@ -22,6 +25,7 @@ public class UserService implements IUserService {
 		Users userPojo = new Users();
 		userPojo.setUsername(loginName);
 		userPojo.setPassword(password);
+		logger.info("Login in with user name:{}",loginName);
 		return this.userMapper.selectByLogin(userPojo);
 	}
 }
