893bb59c6e222154af3548f593ec64ce22e1f0cb
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2018-10-13 16:40:00 +0800

    added swagger config

--- src/main/java/com/ncee/NCEEApplication.java
+++ src/main/java/com/ncee/NCEEApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @MapperScan(basePackages= {"com.ncee.dao.domain"})
 public class NCEEApplication {
 
