96c650fcf1b5d7c82e591c189e0373030096b81c
jungle0127 <arbor@foxmail.com>
2018-10-22 23:08:24 +0800

    updated browser config

--- saa-browser/src/main/java/com/ncee/saa/browser/config/BrowserBeanConfig.java
+++ saa-browser/src/main/java/com/ncee/saa/browser/config/BrowserBeanConfig.java
@@ -5,12 +5,15 @@ import com.ncee.saa.core.validate.respository.ValidateCodeRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class BrowserBeanConfig {
     @Bean
     @ConditionalOnMissingBean(ValidateCodeRepository.class)
-    private ValidateCodeRepository validateCodeRepository(){
+    public ValidateCodeRepository validateCodeRepository(){
         return new ValidateCodeRepositoryImpl();
     }
+
 }
--- saa-browser/src/main/java/com/ncee/saa/browser/controller/AuthController.java
+++ saa-browser/src/main/java/com/ncee/saa/browser/controller/AuthController.java
@@ -1,17 +1,42 @@
 package com.ncee.saa.browser.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import com.ncee.saa.core.properties.SAAProperties;
+import com.ncee.saa.core.support.RestResponse;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/authentication")
 public class AuthController {
-    @PostMapping("/authenticate")
-    public void authenticate(HttpServletRequest request){
-
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Autowired
+    private SAAProperties saaProperties;
+    @RequestMapping("/authenticate")
+    public RestResponse<String> authenticate(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        SavedRequest savedRequest = requestCache.getRequest(request,response);
+        if(savedRequest != null){
+            String targetUrl = savedRequest.getRedirectUrl();
+            logger.info(String.format("target url %s",targetUrl));
+            if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
+                redirectStrategy.sendRedirect(request, response, saaProperties.getLoginPage());
+            }
+        }
+        return new RestResponse<String>("The resource needs authentication, please redirect to login page.");
     }
 
 }
